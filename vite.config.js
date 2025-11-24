68fbfd91c1ef18d0c27752c946a1750d5bd7e28d
jingyanrong <jingyanrong548@gmail.com>
2025-11-24 22:08:03 +0800

    增加亚临界跨临界二氧化碳计算

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // 这里填你的仓库名，注意前后都要有斜杠
+  // 这里的名字要和您的 GitHub 仓库名完全一致
   base: '/Calculation-of-Compressor-Efficiency-pro/', 
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    chunkSizeWarningLimit: 1000,
-  },
-  server: {
-    open: true // 启动时自动打开浏览器
+    // 下面这个配置可以消除那个 1000kB 的黄色警告（通过分包）
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          echarts: ['echarts'], // 把 echarts 单独打包
+          xlsx: ['xlsx']        // 把 xlsx 单独打包
+        }
+      }
+    }
   }
 });
