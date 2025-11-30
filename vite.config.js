7c5d81a92d6ad145a1d6e7e916f39e5a49a92a81
jingyanrong <jingyanrong548@gmail.com>
2025-11-30 10:12:39 +0800

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   // 注意：这里最好填您的仓库名称，前后加斜杠
   // 比如您的仓库名是 Calculation-of-Compressor-Efficiency-pro
-  base: '/Calculation-of-Compressor-Efficiency-pro/', 
+ base: '/Calculation-of-Compressor-Efficiency-pro/',
   
   server: {
     open: true,
